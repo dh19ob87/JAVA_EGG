5f0b770cc451bbcdeb3e74425c834b263e328e7a
Diego Hernando <dh19ob87@gmail.com>
2023-03-25 03:08:32 -0500

    Ejercicio 11 Con la clase time, algunos constructores de Dates están Deprecated

---
+++ guia8Ejercicios/JavaPooEggExer11/src/javapooeggexer11/JavaPooEggExer11.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapooeggexer11;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Scanner;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author diego
+ */
+public class JavaPooEggExer11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in).useDelimiter("\n");
+        System.out.println("Por favor ingrese una fecha. Día, mes y año; respectivamente.");
+        int dia = input.nextInt();
+        int mes = input.nextInt();
+        int anio = input.nextInt();
+        
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd - MM - yyyy");
+        LocalDate fecha = LocalDate.of(anio, Month.of(mes), dia);
+        LocalDate fechaHoy = LocalDate.of(LocalDateTime.now().getYear(), LocalDateTime.now().getMonth(), LocalDateTime.now().getDayOfMonth());
+        System.out.println(fecha.format(formato));
+        System.out.println(fechaHoy.format(formato));
+        System.out.println("Años de diferencia = " + (LocalDateTime.now().getYear() - fecha.getYear()));
+    }
+
+}
