b7b86db893d9a296596304b796e905cef4f62fbb
Diego Hernando <dh19ob87@gmail.com>
2023-04-05 21:40:14 -0500

    Mejorado el cálculo de la edad. Ahora tiene en cuenta los días y meses

--- guia8Ejercicios/JavaPooEggExer11/src/javapooeggexer11/JavaPooEggExer11.java
+++ guia8Ejercicios/JavaPooEggExer11/src/javapooeggexer11/JavaPooEggExer11.java
@@ -7,6 +7,7 @@ package javapooeggexer11;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.Period;
 import java.util.Scanner;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
@@ -31,6 +32,10 @@ public class JavaPooEggExer11 {
         DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd - MM - yyyy");
         LocalDate fecha = LocalDate.of(anio, Month.of(mes), dia);
         LocalDate fechaHoy = LocalDate.of(LocalDateTime.now().getYear(), LocalDateTime.now().getMonth(), LocalDateTime.now().getDayOfMonth());
+        System.out.println("--------Método Period--------");
+        Period fechaPrecisa = Period.between(fecha, fechaHoy);
+        System.out.println("Diferencia --> " + fechaPrecisa.getDays() + " días | " + fechaPrecisa.getMonths() + " meses | " + fechaPrecisa.getYears() + " años");
+        System.out.println("--------Método tradicional--------");
         System.out.println(fecha.format(formato));
         System.out.println(fechaHoy.format(formato));
         System.out.println("Años de diferencia = " + (LocalDateTime.now().getYear() - fecha.getYear()));
