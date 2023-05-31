3d5bfead8f303375c0efdf0d6205c1f0bfd85315
Diego Hernando <dh19ob87@gmail.com>
2023-05-31 01:03:49 -0500

    Ejercicio 3

--- guia11Ejercicios/Guia11Exer2/src/guia11exer2/Guia11Exer2.java
+++ guia11Ejercicios/Guia11Exer2/src/guia11exer2/Guia11Exer2.java
@@ -5,7 +5,11 @@
 package guia11exer2;
 
 import guia11exer2.Entidad.*;
+import guia11exer2.Enumerables.Color;
+import guia11exer2.Enumerables.LetrasConsumo;
 import guia11exer2.Servicio.*;
+import java.util.ArrayList;
+
 /**
  *
  * @author diego
@@ -21,6 +25,18 @@ public class Guia11Exer2 {
         TelevisorServicio tvService = new TelevisorServicio();
         washerService.imprimirData(washerService.crearLavadora());
         tvService.imprimirDatos(tvService.crearTelevisor());
+        ArrayList<Electrodomestico> devices = new ArrayList<>();
+        devices.add(new Lavadora(31, 1000, Color.ROJO, LetrasConsumo.F, 100));
+        devices.add(new Televisor(100, true, 1000, Color.BLANCO, LetrasConsumo.A, 250));
+        devices.add(new Lavadora(20, 1000, Color.NEGRO, LetrasConsumo.B, 20));
+        devices.add(new Televisor(25, false, 1000, Color.BLANCO, LetrasConsumo.F, 250));
+        devices.forEach(electrodomestico -> electrodomestico.toString());
+        devices.forEach(System.out::println);
+        double precioTotalElectrodomesticos = 0;
+        for(Electrodomestico device : devices){
+            precioTotalElectrodomesticos += device.getPrecio();
+        }
+        System.out.println("Precio total de los electrodomésticos en la lista: " + precioTotalElectrodomesticos);
     }
-    
+
 }
